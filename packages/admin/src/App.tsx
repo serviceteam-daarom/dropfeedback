07ca994fb159870111f04ec71ba62aaf18f33576
serviceteam-daarom <serviceteam@daar-om.nl>
2025-08-28 13:01:39 +0200

    refactor(admin): use supabase google oauth

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,5 +1,4 @@
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import {
   BrowserRouter,
   Link,
@@ -11,7 +10,6 @@ import {
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModalRoot } from "@/components/global-modals/modal-root";
 import { QueryClientProvider } from "@/lib/react-query";
-import { CONFIG } from "@/lib/config";
 import { Authenticated } from "@/components/authenticated";
 import { PageLogin } from "@/pages/auth/login";
 import { PageLoginWithEmail } from "@/pages/auth/login/email";
@@ -42,92 +40,90 @@ const App = () => {
         disableTransitionOnChange
       >
         <QueryClientProvider>
-          <GoogleOAuthProvider clientId={CONFIG.googleClientId}>
-            <main className="relative min-h-screen">
-              <ModalRoot />
-              <Routes>
-                <Route
-                  element={
-                    <Authenticated key="protected">
-                      <LayoutCommon>
-                        <Outlet />
-                      </LayoutCommon>
-                    </Authenticated>
-                  }
-                >
-                  <Route index element={<Navigate to="/projects" />} />
+          <main className="relative min-h-screen">
+            <ModalRoot />
+            <Routes>
+              <Route
+                element={
+                  <Authenticated key="protected">
+                    <LayoutCommon>
+                      <Outlet />
+                    </LayoutCommon>
+                  </Authenticated>
+                }
+              >
+                <Route index element={<Navigate to="/projects" />} />
 
-                  <Route path="/projects" element={<Outlet />}>
-                    <Route index element={<PageProjectList />} />
+                <Route path="/projects" element={<Outlet />}>
+                  <Route index element={<PageProjectList />} />
+
+                  <Route
+                    path=":projectId"
+                    element={
+                      <LayoutProject>
+                        <Outlet />
+                      </LayoutProject>
+                    }
+                  >
+                    <Route index element={<Navigate to="feedbacks" />} />
 
                     <Route
-                      path=":projectId"
-                      element={
-                        <LayoutProject>
-                          <Outlet />
-                        </LayoutProject>
-                      }
-                    >
-                      <Route index element={<Navigate to="feedbacks" />} />
-
-                      <Route
-                        path="feedbacks"
-                        element={<PageProjectFeedbackList />}
-                      />
-                      <Route
-                        path="feedbacks/:feedbackId"
-                        element={<PageProjectFeedbackDetail />}
-                      />
-                      <Route path="team" element={<PageProjectTeam />} />
-                      <Route
-                        path="settings"
-                        element={<PageProjectSettings />}
-                      />
-                    </Route>
+                      path="feedbacks"
+                      element={<PageProjectFeedbackList />}
+                    />
+                    <Route
+                      path="feedbacks/:feedbackId"
+                      element={<PageProjectFeedbackDetail />}
+                    />
+                    <Route path="team" element={<PageProjectTeam />} />
+                    <Route
+                      path="settings"
+                      element={<PageProjectSettings />}
+                    />
                   </Route>
-
-                  <Route path="settings" element={<PageSettings />} />
-                  <Route
-                    path="email-verification"
-                    element={<PageEmailVerification />}
-                  />
                 </Route>
 
+                <Route path="settings" element={<PageSettings />} />
                 <Route
-                  element={
-                    <Authenticated
-                      key="auth-pages"
-                      fallback={
-                        <LayoutAuthPage>
-                          <Outlet />
-                        </LayoutAuthPage>
-                      }
-                    >
-                      <Navigate to="/" />
-                    </Authenticated>
-                  }
-                >
-                  <Route path="login" element={<PageLogin />} />
-                  <Route path="login/email" element={<PageLoginWithEmail />} />
-                  <Route path="signup" element={<PageSignup />} />
-                  <Route
-                    path="signup/email"
-                    element={<PageSignupWithEmail />}
-                  />
-                </Route>
-
-                <Route
-                  path="*"
-                  element={
-                    <>
-                      <div>404</div>
-                      <Link to="/">Go home</Link>
-                    </>
-                  }
+                  path="email-verification"
+                  element={<PageEmailVerification />}
                 />
-              </Routes>
-            </main>
-          </GoogleOAuthProvider>
+              </Route>
+
+              <Route
+                element={
+                  <Authenticated
+                    key="auth-pages"
+                    fallback={
+                      <LayoutAuthPage>
+                        <Outlet />
+                      </LayoutAuthPage>
+                    }
+                  >
+                    <Navigate to="/" />
+                  </Authenticated>
+                }
+              >
+                <Route path="login" element={<PageLogin />} />
+                <Route path="login/email" element={<PageLoginWithEmail />} />
+                <Route path="signup" element={<PageSignup />} />
+                <Route
+                  path="signup/email"
+                  element={<PageSignupWithEmail />}
+                />
+              </Route>
+
+              <Route
+                path="*"
+                element={
+                  <>
+                    <div>404</div>
+                    <Link to="/">Go home</Link>
+                  </>
+                }
+              />
+            </Routes>
+          </main>
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </ThemeProvider>
